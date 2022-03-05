35d73f1ec94482649e94aba3164b14145b20bf3e
mahdi bashiri <mahdi94250@gmail.com>
2022-03-05 18:14:11 +0330

    bug fix cpmponents slider & header

--- src/js/script.js
+++ src/js/script.js
@@ -27,22 +27,53 @@ function updateSliderPosition() {
 }
 
 //BTN NEXT & PRIV SLIDER
-btnNext.addEventListener('click', () => {
+function nextSlide() {
   positionImage === sliderItemLength - 1 ? positionImage = 0 : positionImage++;
   updateSliderPosition()
-})
+}
+btnNext.addEventListener('click', nextSlide)
 
-btnPrev.addEventListener('click', () => {
+function prevSlide() {
   positionImage === 0 ? positionImage = sliderItemLength - 1 : positionImage--;
   updateSliderPosition()
-})
+}
+btnPrev.addEventListener('click', prevSlide)
+
+setInterval(() => {
+  nextSlide()
+}, 4000);
 
 //LOCATION IN IMAGE
 sliderItem.forEach(item => {
-  let result = `<span class=" h-2 rounded-full mx-1"></span>`;
+  let result = `<span class=" h-2 rounded-full mx-1 duration-500 ease-in transition-all"></span>`;
   itemPlace.innerHTML += result;
 })
 
 document.addEventListener('DOMContentLoaded', () => {
   updateSliderPosition()
+})
+
+//SECTION HEADER
+const btnNav = document.querySelector('.btnNav')
+const btnNavItem = btnNav.children
+const nav = document.querySelector('.nav')
+
+btnNav.addEventListener('click', () => {
+  if (nav.matches('.hidden')) {
+    nav.classList.replace('hidden', 'block')
+    btnNavItem[0].classList.add('rotate-[45deg]')
+    btnNavItem[0].classList.add('translate-y-[9px]')
+    btnNavItem[1].classList.add('opacity-0')
+    btnNavItem[1].classList.add('-translate-x-4')
+    btnNavItem[2].classList.add('-rotate-[45deg]')
+    btnNavItem[2].classList.add('-translate-y-[9px]')
+  } else {
+    nav.classList.replace('block', 'hidden')
+    btnNavItem[0].classList.remove('rotate-[45deg]')
+    btnNavItem[0].classList.remove('translate-y-[9px]')
+    btnNavItem[1].classList.remove('opacity-0')
+    btnNavItem[1].classList.remove('-translate-x-4')
+    btnNavItem[2].classList.remove('-rotate-[45deg]')
+    btnNavItem[2].classList.remove('-translate-y-[9px]')
+  }
 })
